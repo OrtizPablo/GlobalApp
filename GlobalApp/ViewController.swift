ebf9872e43c4a6646d313515e127bf34a4412712
Pablo Ortiz Rodríguez <pabloortizrodriguez1995@gmail.com>
2017-11-24 20:56:55 +0100

    Creation of Login

--- GlobalApp/ViewController.swift
+++ GlobalApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var user: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,6 +23,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-
+    //MARK: Actions
+    
+    @IBAction func Enter(_ sender: UIButton) {
+        // If the user is a customer
+        if user.text == "Customer" && password.text == "Cust1234"
+        {
+            performSegue(withIdentifier: "CustomerSegue", sender: self)
+        }
+            //If the user is a provider
+        else if user.text == "Provider" && password.text == "Prov1234"
+        {
+            performSegue(withIdentifier: "ProviderSegue", sender: self)
+        }
+            //Error message
+        else
+        {
+            let alertView = UIAlertController(title: "Error", message: "Unknown contact", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertView.addAction(action)
+            self.present(alertView, animated: true, completion: nil)
+        }
+    }
+    
 }
 
